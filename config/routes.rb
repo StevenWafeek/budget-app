73b590db71402a8bf26161deedf85d4e56a0ddef
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-09-12 18:19:09 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { registrations: 'registrations' }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/splash', to: 'splashes#index'
+
+
+  devise_scope :user do
+    authenticated :user do
+      root 'categories#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root "splashes#index", as: :unauthenticated_root
+    end
+  end
+
+  resources :users, only: [:index, :show, :new] do
+    resources :categories, only: [:new, :create, :show, :index, :destroy]
+    resources :entities, only: [:new, :create, :index, :show, :destroy]
+  end
 end
