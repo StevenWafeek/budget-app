67f3cdbeede6aa6f048c76a9d4717ac4ae05a145
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-09-13 21:03:43 +0300

    Update schema and css files

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_124545) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  # add_foreign_key "categories", "users", column: "author_id"
-  # add_foreign_key "entities", "users", column: "author_id"
+  add_foreign_key "categories", "users", column: "author_id"
+  add_foreign_key "entities", "users", column: "author_id"
 end
