047b401114862c724a45b148f37f771c1f90681d
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-09-14 10:09:47 +0300

    Update migration files

--- db/migrate/20230725081612_create_join_table_categories_entities.rb
+++
@@ -1,7 +0,0 @@
-class CreateJoinTableCategoriesEntities < ActiveRecord::Migration[7.0]
-  def change
-    create_join_table :categories, :entities do |t|
-      t.index [:category_id, :entity_id]
-    end
-  end
-end
--- db/migrate/20230725121031_add_user_ref_to_entities.rb
+++
@@ -1,5 +0,0 @@
-class AddUserRefToEntities < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :entities, :author, null: false, foreign_key: { to_table: :users }
-  end
-end
